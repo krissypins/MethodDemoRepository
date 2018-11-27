e517ac726044f2e708ca623e4ee1e7ebceea8329
Krissy <45279097+krissypins@users.noreply.github.com>
2018-11-27 10:26:12 -0500

    Added code to main method and created a method
    Code makes and fills 2 array lists with values 1 to 10. Method combines the two array lists into one larger array list.

--- GitHubMethodPractice.java
+++ GitHubMethodPractice.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
 public class GitHubMethodPractice{
 
 	public static void main(String[]args){
 
+	ArrayList<Integer> list = new ArrayList<>();
+	ArrayList<Integer> list2 = new ArrayList<>();
+
+	for(int i = 1; i <= 10; i++){
+		list.add(i);
+		list2.add(i);
+	}//close for
+
+	combineArr(list, list2);
+
 	}
+
+	public static ArrayList<Integer> combineArr(ArrayList<Integer> arr, ArrayList<Integer> arr2){
+		for(int i = 0; i < arr.size(); i++){
+			arr.add(arr2.get(i));
+		}//close for
+		return(arr);
+	}//close combineArr
 
 }
